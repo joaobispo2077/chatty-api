e607b68a9a3e98e829b186c3d1ff7d71e1a5483a
João Bispo <joaobispo2077@gmail.com>
2021-04-19 10:21:33 -0300

    feat: create index get route and user post route

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,12 @@ const app = express();
 
 const port = 3333;
 
+app.get('/', (req, res, next) => {
+  return res.json({ message: 'Hello chatty api!' });
+});
+
+app.post("/user", (req, res, next) => {
+  return res.json({ message: 'created user with success.' })
+});
+
 app.listen(port, () => console.log(`server is running at port ${port}`));
