ed3807ec8e464f3db75027deca1bce310b68dd0c
João Bispo <joaobispo2077@gmail.com>
2021-04-21 11:27:36 -0300

    feat: added user controller

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
+
+
+class UsersController {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try{
+      const { email } = req.body;
+
+      const usersService = new UsersService();
+
+      const user = await usersService.create(email);
+
+      return res.status(201).json(user);  
+    }catch(err){
+      return res.status(400).json({ error: err.message});
+    }
+  }
+ }
