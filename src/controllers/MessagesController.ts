08b42fa220f02cf3ae0a737f91ea15055a693a7c
João Bispo <joaobispo2077@gmail.com>
2021-04-21 13:46:38 -0300

    chore: added try-catch in create message

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -3,15 +3,21 @@ import { MessagesService } from "../services/MessagesSerivce";
 
 class MessagesController {
   async create(req: Request, res: Response, next: NextFunction){
-    const { text, user_id, admin_id } = req.body;
+    try{
+      const { text, user_id, admin_id } = req.body;
+  
+      const messageService = new MessagesService();
+  
+      const message = await messageService.create({
+        text,
+        user_id,
+        admin_id
+      });
 
-    const messageService = new MessagesService();
-
-    const message = await messageService.create({
-      text,
-      user_id,
-      admin_id
-    })
+      return res.status(201).json(message);
+    }catch(err){
+      return res.status(500).json({error: err.message});
+    }
   }
 }
 
