32561fd7a8714d1136edeb3fd323a941825d48bb
João Bispo <joaobispo2077@gmail.com>
2021-04-22 09:42:02 -0300

    feat: connect client-side and server-side with websockets

---
+++ src/http.ts
@@ -0,0 +1,35 @@
+import express from  'express';
+
+import path from 'path';
+
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
+import {routes} from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set("views", path.join(__dirname, '..', 'public'));
+app.engine("html", require('ejs').renderFile);
+app.set("view engine", "html")
+
+app.get('/pages/chat/client', (req, res) => {
+  res.render('html/client.html');
+});
+
+const http = createServer(app); // creating http protocol
+const io = new Server(http); // creating websocket protocol
+
+io.on("connection", (socket: Socket) => {
+  console.log("conected...", socket.id);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,7 @@
-import express from  'express';
-
-import path from 'path';
-
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-
-import {routes} from './routes';
-
-import './database';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set("views", path.join(__dirname, '..', 'public'));
-app.engine("html", require('ejs').renderFile);
-app.set("view engine", "html")
-
-app.get('/pages/chat/client', (req, res) => {
-  res.render('html/client.html');
-});
-
-const http = createServer(app); // creating http protocol
-const io = new Server(http); // creating websocket protocol
-
-io.on("connection", (socket: Socket) => {
-  console.log("conected...", socket.id);
-});
+import { http } from './http';
+import './websockets/client';
+import './websockets/admin';
 
 const port = 3333;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
 
 http.listen(port, () => console.log(`server is running at port ${port}`));
