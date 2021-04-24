94361663d43116aaa18cf5323daebbf77a563fd7
João Bispo <joaobispo2077@gmail.com>
2021-04-24 12:00:36 -0300

    feat:added clean users in chat

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -53,6 +53,16 @@ class ConnectionsService {
     
     return connection;
   };
+
+  async updateAdminID(user_id: string, admin_id: string) {
+    await this.connectionsRepository
+    .createQueryBuilder().update(Connection)
+    .set({admin_id})
+    .where("user_id = :user_id", {
+      user_id
+    })
+    .execute();
+  }
 }
 
 export { ConnectionsService };
